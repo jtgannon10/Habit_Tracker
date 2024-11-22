eeced5e9529a52779124f693a3fedf3438135428
jtgannon10 <jtgannon12@gmail.com>
2024-11-22 16:03:04 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -738,6 +738,6 @@ def download_excel():
         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     )
 
-# if __name__ == '__main__':
-#     # app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 #     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
