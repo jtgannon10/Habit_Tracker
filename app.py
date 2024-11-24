bb43fa57dce857ac1a7390b0d3159a0be9d5afab
jtgannon10 <jtgannon12@gmail.com>
2024-11-24 00:44:46 -0500

    Optimize for Render

--- app.py
+++ app.py
@@ -753,5 +753,5 @@ def download_excel():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
-#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
